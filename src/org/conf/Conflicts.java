ed8f713e32883283048ff9a446b9c156af84af50
PrabhaPapu07 <prabhakaran.m91@gmail.com>
2018-10-27 22:20:17 +0530

    created a class

---
+++ src/org/conf/Conflicts.java
@@ -0,0 +1,20 @@
+package org.conf;
+
+public class Conflicts {
+	
+		public static void main(String[] args) {
+		int oddCount = 0,evenCount=0;
+		for (int i = 1; i <= 100; i++) {
+		if (i % 2 == 1) {
+		oddCount= oddCount + i;
+		}
+		else {
+		evenCount=evenCount+i;
+		}
+		}
+		System.out.println("Count of odd number is "+oddCount);
+		System.out.println("Count of even number is "+evenCount);
+		}
+		}
+
+
